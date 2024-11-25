a03dfbfe5216ffcb0471d463a26cc787a10b2e92
Jimmy :) <146484453+zsnowboarder@users.noreply.github.com>
2024-11-25 14:21:48 -0800

    Update eim_api.py

--- eim_api.py
+++ eim_api.py
@@ -4,6 +4,6 @@ import google.generativeai as genai
 st.title("eIM + Offence Classifier + Summarizer")
 
 api_key = st.secrets["gsc_connections"]["api_key"]
-genai.configure(api_key)
+genai.configure(api_key=api_key)
 
-st.write(api_key) 
+st.write(api_key)
