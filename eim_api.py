c6cbb4419f8a7f87bb55d93a34c144f1c04dcf95
Jimmy :) <146484453+zsnowboarder@users.noreply.github.com>
2024-11-28 22:17:58 -0800

    Update eim_api.py

--- eim_api.py
+++ eim_api.py
@@ -64,8 +64,8 @@ def generate_xml():
  
 def generate(inst_text, prompt_text, start_time):
  elapsed_time = time.time() - start_time
- st.write("now" + time.time())
- st.write("start" + start_time)
+ st.write("now" + str(time.time()))
+ st.write("start" + str(start_time))
  if elapsed_time > 0.017528057098388672:
   model = genai.GenerativeModel(
    model_name="gemini-1.5-pro",
